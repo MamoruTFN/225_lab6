b553f8ec3938ebeebedf0eb8be10450bd93605d9
MamoruTFN <paweenwich.t@ku.th>
2021-03-18 16:17:09 +0700

    ground

--- 6320502461_6_5.c
+++ 6320502461_6_5.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
 
+int main()
+{
+    int w,h,sum=0,t,ans;
+    scanf("%d %d",&w,&h);
+    int a[w];
+    t=w*h;
+
+    for(int i=0;i<h;i++)
+    {
+        for(int j=0;j<w;j++)
+        {
+            scanf("%d",&a[j]);
+            sum+=a[j];
+        }
+    }
+
+    ans=t-(t%sum);
+    printf("%d",ans);
+
+    return 0;
+}
