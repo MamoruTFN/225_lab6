b9531e870762dac81b2d1472f364bd50ed654341
MamoruTFN <paweenwich.t@ku.th>
2021-03-18 14:59:47 +0700

    เกมทายตัวเลข

--- 6320502461_6_2.c
+++ 6320502461_6_2.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
 
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a,b,c1=0,c2=0,a2[n],b2[n];
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    for(int i=0;i<n;i++)
+    {
+        a2[i]=a%10;
+        a/=10;
+        b2[i]=b%10;
+        b/=10;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a2[i]==b2[i])
+        {
+            c1++;
+        }
+        else
+        {
+            c2++;
+        }
+    }
+    printf("%d %d",c1,c2);
+
+    return 0;
+}
