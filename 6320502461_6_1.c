3d90f76a8d3ac131b66694f359cab5a4aac8eae2
MamoruTFN <paweenwich.t@ku.th>
2021-03-18 16:06:27 +0700

    sort1

--- 6320502461_6_1.c
+++ 6320502461_6_1.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    char a[n][1000];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%s",&a[n]);
+    }
+
+    return 0;
+}
