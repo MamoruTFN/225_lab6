1a78b504676533a8b6d29624e93e5f371e7254ec
MamoruTFN <paweenwich.t@ku.th>
2021-03-18 16:59:28 +0700

    drawing machine

--- 6320502461_6_6.c
+++ 6320502461_6_6.c
@@ -1,1 +1,87 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int n,u=0,r=0;
+    scanf("%d",&n);
+    char dm[n][n],o[250];
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            dm[i][j]='.';
+        }
+    }
+
+    scanf("%s",o);
+
+    for(int i=0;i<strlen(o);i++)
+    {
+        if(o[i]=='D')
+        {
+            if(dm[u][r]=='.'||dm[u][r]=='|')
+            {
+                dm[u][r]='|';
+                u--;
+            }
+            else
+            {
+                dm[u][r]='+';
+                u--;
+            }
+
+        }
+        else if(o[i]=='U')
+        {
+            if(dm[u][r]=='.'||dm[u][r]=='|')
+            {
+                dm[u][r]='|';
+                u++;
+            }
+            else
+            {
+                dm[u][r]='+';
+                u++;
+            }
+        }
+        else if(o[i]=='R')
+        {
+            if(dm[u][r]=='.'||dm[u][r]=='-')
+            {
+                dm[u][r]='-';
+                r++;
+            }
+            else
+            {
+                dm[u][r]='+';
+                r++;
+            }
+        }
+        else if(o[i]=='L')
+        {
+            if(dm[u][r]=='.'||dm[u][r]=='-')
+            {
+                dm[u][r]='-';
+                r--;
+            }
+            else
+            {
+                dm[u][r]='+';
+                r--;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%c",dm[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
